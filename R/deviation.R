478946e79197e9a462a6018fcc0a069fd75a7045
bz1013 <hntek@yahoo.com>
2016-12-17 16:04:03 -0800

    test cases completed

--- R/deviation.R
+++ R/deviation.R
@@ -8,12 +8,16 @@
 #' @examples
 #' standard_deviation(c(2,6,7,8))
 standard_deviation <- function(x) {
+  if (is.null(x)){
+    stop("x should contain numbers and should not be null")
+  }
+
   n <- length(x)
-  mean = sum(x) / n
-  ssq <- sum((x-mean)^2)
+
+  tryCatch({
+  mean = sum(x, na.rm = TRUE) / n
+  ssq <- sum((x-mean)^2, na.rm = TRUE)
   stddev = sqrt(ssq/n)
   return(stddev)
+  }, error=function(e) stop("x does not contain numbers and thus a NaN was returned"))
 }
-
-
-# standard_error <- function(x) standard_deviation(x)/sqrt(length(x))
--- tests/testthat/test_deviation.R
+++ tests/testthat/test_deviation.R
@@ -1,24 +1,55 @@
 context("Testing the deviation package")
 
-test_that("Positive numbers should return to a positive standart deviation", {
+test_that("Positive numbers should return to a positive standard deviation", {
   expect_equal(standard_deviation(c(1,2,3,4)), 1.118034)
-  expect_equal(standard_deviation(c(1000,500.800,1000)), 235.325136)
+  expect_equal(standard_deviation(c(1000,500,800,1000)), 204.633819)
   expect_equal(standard_deviation(c(0,100,4)), 46.226495)
+  expect_equal(standard_deviation(c(1,1,1,1)), 0)
 })
 
-# !!!Test cases are not complete!!!!
+test_that("Positive numbers should return to a positive standard error", {
+  expect_equal(standard_error(c(1,2,3,4)), 0.559017)
+  expect_equal(standard_error(c(1000,500,800,1000)), 102.316909)
+  expect_equal(standard_error(c(0,100,4)), 26.6888796)
+  expect_equal(standard_error(c(1,1,1,1)), 0)
+})
 
-#test_that("expect a number not a string", {
-#  expect_that(fahr_to_celsius("A"), throws_error())
-#  expect_that(fahr_to_kelvin("A"), throws_error())
-#  expect_that(celsius_to_fahr("A"), throws_error())
-#  expect_that(celsius_to_kelvin("A"), throws_error())
-#  expect_that(kelvin_to_celsius("A"), throws_error())
-#  expect_that(kelvin_to_fahr("A"), throws_error())
-#
-#})
+test_that("Negative numbers should return to a positive standard deviation", {
+  expect_equal(standard_deviation(c(-1,-2,-3,-4)), 1.118034)
+  expect_equal(standard_deviation(c(-1000,-500,-800,-1000)), 204.633819)
+  expect_equal(standard_deviation(c(0,-100,-4)), 46.226495)
+})
 
-#test_that("kelvin cannot be negative", {
-#  expect_warning(kelvin_to_fahr(-10),"kelvin cannot be negative")
-#  expect_warning(kelvin_to_celsius(-10),"kelvin cannot be negative")
-#})
+test_that("Standard error should be smaller than or equal to standard deviation", {
+  expect_that(standard_deviation(c(1,2,3,4))>=standard_error(c(1,2,3,4)), is_true())
+  expect_that(standard_deviation(c(-1,-2,-3,-4))>=standard_error(c(-1,-2,-3,-4)), is_true())
+})
+
+test_that("Negative numbers should return to a positive standard error", {
+  expect_equal(standard_error(c(-1,-2,-3,-4)), 0.559017)
+  expect_equal(standard_error(c(-1000,-500,-800,-1000)), 102.316909)
+  expect_equal(standard_error(c(0,-100,-4)), 26.6888796)
+})
+
+test_that("NA/NaN should be removed", {
+  expect_equal(standard_deviation(c(1,2,3,NA)), 0.8291562)
+  expect_equal(standard_deviation(c(NaN,500,800,1000)), 243.3490291)
+})
+
+test_that("NA/NaN should be removed", {
+  expect_equal(standard_error(c(1,2,3,NA)), 0.4145781)
+  expect_equal(standard_error(c(NaN,500,800,1000)), 121.6745145)
+})
+
+test_that("empty list should produce an error",{
+expect_that(standard_deviation(c()),throws_error())
+expect_that(standard_error(c()),throws_error())
+})
+
+test_that("the functions take only number",{
+  expect_that(standard_deviation(c(1,2, "a")), throws_error())
+  expect_that(standard_deviation(c("a","b", "c")), throws_error())
+  expect_that(standard_error(c(1,2, "a")), throws_error())
+  expect_that(standard_error(c("a","b", "c")), throws_error())
+})
+
